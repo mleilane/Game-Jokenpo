9753ccd171131653c453a6f64f08ee9d2e4a7068
mleilane <leilane94silva@yahoo.com>
2024-05-15 23:20:14 -0300

    javascript finalizado, aplicação da logica do jogo.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
-// 1º precisamos saber quando o usuario clicar em um botão
-const draw = document.querySelector(".draw"); //mapeando o elemento HTML que vai receber o resultado do empate
+//mapeando os elementos HTML
+const resultgame = document.querySelector(".resultgame"); //mapeando o elemento HTML (pela classe) que vai exibir o resultado (empate, perde ou ganha)
+const playerChoiceElement = document.querySelector("#player-choice"); // mapeando o elemento HTML (pelo ID) que vai receber a escolha do usuario
+const machineChoiceElement = document.querySelector("#machine-choice"); // mapeando o elemento HTML (pelo ID) que vai receber a escolha da maquina
+const yourScoreElement = document.querySelector(".your-score span"); //mapeando o elemento HTML (pela classe) que vai exibir a pontuacao total do usuario
+const machineScoreElement = document.querySelector(".machine-score span"); // mapeando o elemento HTML (pela classe) que vai exibir a pontuacao total da maquina
 
+let yourScore = 0;
+let machineScore = 0;
+
+/*
+humanScore -> Camel case
+GAME_OPTIONS -> snake case  em ENUM usamos esse formato
+*/
+
+// ENUM
+const GAME_OPTIONS = {
+  PEDRA: "PEDRA",
+  PAPEL: "PAPEL",
+  TESOURA: "TESOURA",
+};
+
+// 1º precisamos saber quando o usuario clicar em um botão
 //função para trazer a aescolha do usuario
 const playHuman = (humanChoice) => {
   //console.log(humanChoice)
@@ -10,7 +30,11 @@ const playHuman = (humanChoice) => {
 
 //função para trazer a escolha da maquina
 const playMachine = () => {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = [
+    GAME_OPTIONS.PEDRA,
+    GAME_OPTIONS.PAPEL,
+    GAME_OPTIONS.TESOURA,
+  ];
   const randomNumber = Math.floor(Math.random() * 3); //Math.random() da numeros aleatorios  /Math.floor arredonda o nº para baixo
 
   return choices[randomNumber];
@@ -20,8 +44,24 @@ const playMachine = () => {
 const playTheGame = (human, machine) => {
   console.log("Humano: " + human + " Maquina: " + machine);
 
+  //atualiza a escolha no HTML
+  playerChoiceElement.innerHTML = human;
+  machineChoiceElement.innerHTML = machine;
+
+  //criando a logica do jogo
   if (human === machine) {
-    draw.innerHTML = "Deu empate!";
+    resultgame.innerHTML = "Deu empate! 😕";
+  } else if (
+    (human === GAME_OPTIONS.PAPEL && machine === GAME_OPTIONS.PEDRA) ||
+    (human === GAME_OPTIONS.PEDRA && machine === GAME_OPTIONS.TESOURA) ||
+    (human === GAME_OPTIONS.TESOURA && machine === GAME_OPTIONS.PAPEL)
+  ) {
+    resultgame.innerHTML = "Você Ganhou! 😍";
+    yourScore++;
+    yourScoreElement.innerHTML = yourScore;
+  } else {
+    resultgame.innerHTML = "Você Perdeu! 😢";
+    machineScore++;
+    machineScoreElement.innerHTML = machineScore;
   }
 };
-
